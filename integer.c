f84253e40c7b8188ed682393ad6d059ae6af0de9
Drishti-byte <drishtibansal06@gmail.com>
2024-08-03 12:26:30 +0530

    Add files via upload

---
+++ integer.c
@@ -0,0 +1,10 @@
+/* program to print an integer entered by the user*/
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    printf("Number is %d",num);
+    return 0;
+}
