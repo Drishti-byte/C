83bbb8d58a9eb3eb6c8d6f9544d79d28840e1ab6
Drishti-byte <drishtibansal06@gmail.com>
2024-09-04 15:06:32 +0530

    Add files via upload

---
+++ if6.c
@@ -0,0 +1,13 @@
+/*program to check if a person is retired or not*/
+#include<stdio.h>
+int main()
+{
+    int age,gen,male,female;
+    printf("Enter gender and age:");
+    scanf("%d %d",&gen,&age);
+    if(male && age >= 60)
+    printf("\nYou are retired\n");
+    if(female && age >= 56)
+    printf("\nYou are retired\n");
+    return 0;
+}
