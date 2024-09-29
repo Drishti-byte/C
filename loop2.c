3c14d0caef631c6addbe71fd694ca1222c2890d3
Drishti-byte <drishtibansal06@gmail.com>
2024-09-29 16:59:05 +0530

    Add files via upload

---
+++ loop2.c
@@ -0,0 +1,11 @@
+/*program to print numbers from 6 to 24 using loop*/
+#include<stdio.h>
+int main()
+{
+    int n;
+    for( n=6; n <= 24; n += 3)
+    {
+        printf("\n%d",n);
+    }
+    return 0;
+}
