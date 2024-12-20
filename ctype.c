9b62b7802c0c96400fa09b3efe7f0cb541ccbd0c
Drishti-byte <drishtibansal06@gmail.com>
2024-12-20 15:20:07 +0530

    Add files via upload

---
+++ ctype.c
@@ -0,0 +1,18 @@
+/*program to use the functions of ctype.h file*/
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char ch;
+    ch = getchar();
+    printf("\nEntered character is %c",ch);
+    printf("\nUpper Case %c",toupper(ch));
+    printf("\nLower Case %c",tolower(ch));
+    printf("\n%c",isupper(ch));
+    printf("\n%c",islower(ch));
+    char str[10] = "12345";
+    int s=0;
+    s =atoi(str);
+    printf("\n%d",s);
+    return 0;
+}
